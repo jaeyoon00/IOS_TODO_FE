2c2fab5ce762925fabd56a317c6bd8e41a0e4629
jaeyoon00 <qwero55@naver.com>
2024-06-16 17:20:17 +0900

    친구목록 1차 작업

--- Todo/controller_jaeyoon/MyFriendsViewController.swift
+++ Todo/controller_jaeyoon/MyFriendsViewController.swift
@@ -7,11 +7,61 @@
 
 import UIKit
 
-class MyFriendsViewController : UIViewController {
-    lazy var tableView = UITableView(frame: .zero, style: .insetGrouped)
+class MyFriendsViewController : UIViewController, UITableViewDataSource, UITableViewDelegate {
     
-    @IBOutlet weak var MyFriendsTableView: UITableView!
+    var MyFriednsView = UITableView()
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        addMyFriendTitle()
+        
+        MyFriednsView.dataSource = self
+        MyFriednsView.delegate = self
+        MyFriednsView.register(UITableViewCell.self, forCellReuseIdentifier: "myCell")
+        
+        view.addSubview(MyFriednsView)
+        
+        MyFriednsView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            MyFriednsView.topAnchor.constraint(equalTo: view.topAnchor, constant: 200),
+            MyFriednsView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            MyFriednsView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            MyFriednsView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -80)
+                ])
+    }
+    
+     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+        
+     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+            
+        cell.textLabel?.text = "김재윤"
+            
+        return cell
+    }
+    private func addMyFriendTitle() {
+        let titleImage = UIImageView()
+        titleImage.image = UIImage(named: "친구목록")
+        titleImage.contentMode = .scaleAspectFit
+        titleImage.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(titleImage)
+        
+        NSLayoutConstraint.activate([
+            titleImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            titleImage.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            titleImage.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30)
+        ])
+    }
    
 }
 
+
+
+
+#Preview {
+    let vc = MyFriendsViewController()
+    return vc
+}
